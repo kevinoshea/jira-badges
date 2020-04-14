a4ab6070b39881ddb7bb5dd35bfba3902e51ff2c
Kevin O'Shea <45778337+kevinoshea@users.noreply.github.com>
2020-04-14 21:47:26 +1000

    Added jira badges script.

---
+++ jira-badges.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         Jira PR Badges
+// @version      1.0
+// @description  Adds badges to tickets in jira scrum board to indicate pull request status
+// @match        https://jira.orionhealth.global/secure/RapidBoard.jspa?rapidView=3198*
+// ==/UserScript==
+
+const BADGE_TYPES = {
+    OPEN: 'OPEN',
+    IN_PROGRESS: 'IN_PROGRESS',
+    MERGED: 'MERGED',
+};
+
+const BADGE_CLASSES = {
+    OPEN: 'aui-lozenge-current',
+    IN_PROGRESS: 'aui-lozenge-complete',
+    MERGED: 'aui-lozenge-success',
+};
+
+const BADGE_TEXT = {
+    OPEN: 'OPEN',
+    IN_PROGRESS: 'IN PROGRESS',
+    MERGED: 'MERGED',
+};
+
+const addBadge = (ticket, keySelector) => {
+    const ticketKey = ticket.querySelector(keySelector).innerText; // eg MEM-1234
+
+    fetch(`https://jira.orionhealth.global/rest/api/latest/issue/${ticketKey}`).then(response => {
+        return response.json();
+
+    }).then(responseJson => {
+        const ticketId = responseJson.id; // eg 123456
+        return fetch(`https://jira.orionhealth.global/rest/dev-status/1.0/issue/detail?issueId=${ticketId}&applicationType=stash&dataType=pullrequest`);
+
+    }).then(response => {
+        return response.json();
+
+    }).then(responseJson => {
+        const badgesAdded = [];
+        const prList = responseJson?.detail?.[0]?.pullRequests;
+        const badges = new Set();
+        prList.forEach(pr => {
+            if (pr.status === 'OPEN') {
+                if (!pr.reviewers?.length) {
+                    badges.add(BADGE_TYPES.OPEN);
+                } else {
+                    badges.add(BADGE_TYPES.IN_PROGRESS);
+                }
+            } else if (pr.status === 'MERGED') {
+                badges.add(BADGE_TYPES.MERGED);
+            }
+        });
+        badges.forEach(badge => {
+            ticket.innerHTML += ` <span class="aui-lozenge ${BADGE_CLASSES[badge]}">${BADGE_TEXT[badge]}</span>`
+        });
+
+    }).catch((err) => console.error(err));
+};
+
+const addBadges = () => {
+    const tickets = document.querySelectorAll('.ghx-issue-fields');
+    const parentTickets = document.querySelectorAll('.ghx-heading');
+    tickets.forEach(ticket => addBadge(ticket, '.ghx-key'));
+    parentTickets.forEach(parentTicket => addBadge(parentTicket, '.ghx-parent-key'));
+};
+
+const main = () => {
+    setTimeout(function() {
+        const ticketsNotLoadedYet = !document.querySelector('.ghx-issue-fields');
+        if (ticketsNotLoadedYet) {
+            main();
+        } else {
+            addBadges();
+        }
+    }, 1000);
+};
+
+main();
